f945a5fe545a1564166443537dbea9b96d1b326e
mira67 <qliu.hit@gmail.com>
2017-05-01 17:47:48 -0600

    merge

--- code/analysis/testbic.py
+++
@@ -1,9 +0,0 @@
-import multiprocessing
-
-def funSquare(num):
-    return num ** 2
-
-if __name__ == '__main__':
-    pool = multiprocessing.Pool(2)
-    results = pool.map(funSquare, range(8))
-    print(results)
