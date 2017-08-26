512d6a01f5a264617d02654fcbacf5ab2c687886
mira67 <qliu.hit@gmail.com>
2017-08-26 17:46:43 -0600

    clean and progress on model fault code

--- code/python/pv_estFault.py
+++ code/python/pv_estFault.py
@@ -6,8 +6,10 @@
 
 import pymysql.cursors
 import numpy as np
-from pandas import DataFrame
+import pandas as pd
 from sklearn.linear_model import LinearRegression
+import time
+import multiprocessing as mp
 
 #Make database connetion
 db = pymysql.connect(host='localhost',
@@ -20,6 +22,15 @@ db = pymysql.connect(host='localhost',
 
 cursor=db.cursor()
 
+#Parameters configuration
+startDTModel = '2016-01-01'
+endDTModel = '2016-03-31'
+
+startDTTest = '2016-06-01'
+endDTTest = '2016-06-30'
+
+timeRg = ['10:00','16:00'];#use pandas to get data within this range
+
 """
 Step 1: Extract data from database, table-hlx
 Input: String Info: hlxID, strID, Datetime: startDT,endDT
@@ -28,7 +39,7 @@ Output: String current
 def queryStrData(hlxID, strID, startDT,endDT):
     
     sql = """SELECT {} FROM pingyuan.hlx WHERE combinerbox = '{}' 
-            AND riqi BETWEEN '{}' AND '{}';"""
+            AND data_date BETWEEN '{}' AND '{}' AND TIME(data_date) BETWEEN '10:00'AND '16:00';"""
     sqlSts = sql.format(strID, hlxID, startDT,endDT)
     print(sqlSts)
     try:
@@ -37,7 +48,7 @@ def queryStrData(hlxID, strID, startDT,endDT):
         db.commit()
         
         #collect query data
-        strCurrent = DataFrame(cursor.fetchall())
+        strCurrent = pd.DataFrame(cursor.fetchall())
         print(strCurrent.head())
     except:
         # Rollback in case there is any error
@@ -55,8 +66,9 @@ Input: Features: FeatureList, Datetime: startDT,endDT
 Output: Features array
 """
 def queryFeaData(FeatureList, startDT,endDT):
-    sql = """SELECT {} FROM pingyuan.qxz WHERE riqi BETWEEN '{}' AND '{}';"""
-    sqlSts = sql.format(FeatureList,startDT,endDT)
+    sql = """SELECT FS1,Fs2,Fs1m,Fs2m,Wv,Wd,Sd,T0 FROM pingyuan.qxz WHERE data_date BETWEEN '{}' AND '{}'
+    AND TIME(data_date) BETWEEN '10:00'AND '16:00';"""
+    sqlSts = sql.format(startDT,endDT)
     print(sqlSts)
     try:
         cursor = db.cursor()
@@ -64,7 +76,7 @@ def queryFeaData(FeatureList, startDT,endDT):
         db.commit()
         
         #collect query data
-        features = DataFrame(cursor.fetchall())
+        features = pd.DataFrame(cursor.fetchall())
         print(features.head())
     except:
         # Rollback in case there is any error
@@ -80,33 +92,70 @@ def queryFeaData(FeatureList, startDT,endDT):
 #consider to also write to files for later easier access?
 
 #Step 3: Build model for individual string
-def strPowerModel(Features, StrCurrent):
-    #Libear model
+def strPowerModel(Features,stringCurrent):
+    #Build libear model
     lm = LinearRegression()
-    lm.fit(Features,StrCurrent)
+    lm.fit(Features,stringCurrent)
     return lm
     
 #Step 4: Fault detection for individual string
-def strFaultDetection(Features, StrCurrent):
-    lm = strPowerModel(Features, StrCurrent)
+def strFaultDetection(hlxID, strID, FeatureList, startDT,endDT):
+    #Get data
+    stringCurrent = queryStrData(hlxID, strID, startDT,endDT)
+    Features = queryFeaData(FeatureList, startDT,endDT)
+    
+    #Build Model
+    #lm = strPowerModel(Features, stringCurrent)
+   
     """using model to check new data for faults
         grab test data from database, using last 10 days in June
         Method 1: directly compare difference error > 10%
     """
-    testX = [000]#update to real data from database
-    testY = [000]
+    
+    #Get test data for this string [2016-06-01, 2016-06-30] data
+    """
+    testX = queryFeaData(FeatureList, startDTTest,endDTTest)
+    testY = queryStrData(hlxID, strID, startDTTest,endDTTest)
     predY = lm.predict(testX)
     resErr = (testY-predY)/testY*100
+    print(resErr)
+    return resErr
+    """
     #set resErr > 10 to Fault label in restult file
+    
     
 #Step 5: Summarize fault detection results for ground truth comparison
 def rankFaultString():
     pass
     
-
+def test(lists):
+    print(lists)
+    
 #Main
 def main():
-    pass
+    dataPath = 'E:/myprojects/pv_detection/code/code/python/testData.xlsx'
+    strInfo = pd.read_excel(dataPath).values.tolist(); 
+    #strings = map(str, strInfo)#seems only string list works for pool map
+    #print(strInfo)
+    
+    testData = strInfo[0]
+    hlxID = testData[0]
+    strID = 'I'+str(testData[1])
+    FeatureList = ['FS1','Fs2','Fs1m','Fs2m','Wv','Wd','Sd','T0']
+    strFaultDetection(hlxID, strID, FeatureList, startDTModel,endDTModel)
+    
+    #profiling 1
+    '''
+    start = time.time()
+    #patternDetection('1191278995')
+    with mp.Pool(3) as pool:
+        results = pool.map(test, strings)
+    end = time.time()
+    runtime = end - start
+    msg = "Fault Detection Multi-Processing Took {time} seconds to complete"
+    print(msg.format(time=runtime))
+    '''
+    
 
 if __name__ == "__main__":
     main()
